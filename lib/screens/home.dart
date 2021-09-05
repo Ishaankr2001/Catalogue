64575fae159084d42ddfc4bb23df296e5676547c
Ishaankr2001 <kumar.ishaan2001@gmail.com>
2021-09-05 19:05:38 +0530

    Login Linked with Home Screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  int age = 20;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalogue"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+            'Hey There\n\nI am $age years old.',
+          ),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
